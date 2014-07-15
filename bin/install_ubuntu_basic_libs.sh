8eec9d4d3df5f14da18e81b653a874888c73684e
Ulric Qin <ulric.qin@gmail.com>
2014-07-15 12:10:19 +0800

    add install_ubuntu_basic_libs.sh

---
+++ bin/install_ubuntu_basic_libs.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+apt-get install -y vim git ctags tree tar bzr libcurl-ocaml-dev zip curl lrzsz libxml2-dev libxslt1-dev build-essential debootstrap quota libpqxx3-dev libmysqlclient-dev
+
+echo "done:)"
+
